90de00b03d02819108ee9a77a43ee743619732cd
L Toal <lc790935@dal.ca>
2018-05-09 13:34:39 -0300

    Added text box, button, and method for swapping text.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void swapText(View view){
+        EditText textBox = findViewById(R.id.editText);
+        String desiredText = textBox.getText().toString();
+        TextView defaultBox = findViewById(R.id.textView);
+        defaultBox.setText(desiredText);
+        textBox.setText("");
     }
 }
